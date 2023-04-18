e80bffa374efd43f4a5a26d549852649208b129f
Bagualisson <alisson1397@hotmail.com>
2023-04-18 12:42:18 -0300

    [TMP]vayu: Use lineage stuffs

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_vayu.mk
+    $(LOCAL_DIR)/lineage_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    havoc_vayu-eng \
-    havoc_vayu-userdebug \
-    havoc_vayu-user
+    lineage_vayu-eng \
+    lineage_vayu-userdebug \
+    lineage_vayu-user
